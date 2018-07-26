e5322a9af3601a01fcf8ee16f345e8ced6f09338
oliviamarquis@gmail.com <demouser@MacBook-Pro.local>
2018-07-26 18:27:29 -0400

    welcome page and stuff

--- main.py
+++ main.py
@@ -82,61 +82,61 @@ jinja_current_dir = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
-    welcome_template = jinja_current_dir.get_template("Templates/index.html")
+    welcome_template = jinja_current_dir.get_template("Templates/welcome1.html")
     self.response.write(welcome_template.render())
-    user = users.get_current_user()
-    # If the user is logged in...
-    if user:
-      email_address = user.nickname()
-      cssi_user = CssiUser.get_by_id(user.user_id())
-      signout_link_html = '<a href="%s">sign out</a>' % (
-          users.create_logout_url('/'))
-      # If the user has previously been to our site, we greet them!
-      if cssi_user:
-        self.response.write('''
-            Welcome %s %s (%s)! <br> %s <br>''' % (
-              cssi_user.first_name,
-              cssi_user.last_name,
-              email_address,
-              signout_link_html))
-      # If the user hasn't been to our site, we ask them to sign up
-      else:
-        self.response.write('''
-            Welcome to our site, %s!  Please sign up! <br>
-            <form method="post" action="/">
-            <input type="text" name="first_name">
-            <input type="text" name="last_name">
-            <input type="text" name="user_name">
-            <input type="text" name="password">
-            <input type="submit">
-            </form><br> %s <br>
-            ''' % (email_address, signout_link_html))
-    # Otherwise, the user isn't logged in!
-    else:
-      self.response.write('''
-        Please log in to use our site! <br>
-        <a href="%s">Sign in</a>''' % (
-          users.create_login_url('/')))
-
-  def post(self):
-    bye_template = jinja_current_dir.get_template("Templates/Home.html")
-    self.response.write(bye_template.render())
-    user = users.get_current_user()
-    if not user:
-      # You shouldn't be able to get here without being logged in
-      self.error(500)
-      return
-    cssi_user = CssiUser(
-        first_name=self.request.get('first_name'),
-        last_name=self.request.get('last_name'),
-        id=user.user_id())
-    cssi_user.put()
-    self.response.write('Thanks for signing up, %s!' %
-        cssi_user.first_name)
-
-  def post(self):
-      username = self.request.get("user_name")
-      newuser = CssiUser(user_name = username, last_activity = datetime.datetime.now())
+  #   user = users.get_current_user()
+  #   # If the user is logged in...
+  #   if user:
+  #     email_address = user.nickname()
+  #     cssi_user = CssiUser.get_by_id(user.user_id())
+  #     signout_link_html = '<a href="%s">sign out</a>' % (
+  #         users.create_logout_url('/'))
+  #     # If the user has previously been to our site, we greet them!
+  #     if cssi_user:
+  #       self.response.write('''
+  #           Welcome %s %s (%s)! <br> %s <br>''' % (
+  #             cssi_user.first_name,
+  #             cssi_user.last_name,
+  #             email_address,
+  #             signout_link_html))
+  #     # If the user hasn't been to our site, we ask them to sign up
+  #     else:
+  #       self.response.write('''
+  #           Welcome to our site, %s!  Please sign up! <br>
+  #           <form method="post" action="/">
+  #           <input type="text" name="first_name">
+  #           <input type="text" name="last_name">
+  #           <input type="text" name="user_name">
+  #           <input type="text" name="password">
+  #           <input type="submit">
+  #           </form><br> %s <br>
+  #           ''' % (email_address, signout_link_html))
+  #   # Otherwise, the user isn't logged in!
+  #   else:
+  #     self.response.write('''
+  #       Please log in to use our site! <br>
+  #       <a href="%s">Sign in</a>''' % (
+  #         users.create_login_url('/')))
+  #
+  # def post(self):
+  #   bye_template = jinja_current_dir.get_template("Templates/Home.html")
+  #   self.response.write(bye_template.render())
+  #   user = users.get_current_user()
+  #   if not user:
+  #     # You shouldn't be able to get here without being logged in
+  #     self.error(500)
+  #     return
+  #   cssi_user = CssiUser(
+  #       first_name=self.request.get('first_name'),
+  #       last_name=self.request.get('last_name'),
+  #       id=user.user_id())
+  #   cssi_user.put()
+  #   self.response.write('Thanks for signing up, %s!' %
+  #       cssi_user.first_name)
+  #
+  # def post(self):
+  #     username = self.request.get("user_name")
+  #     newuser = CssiUser(user_name = username, last_activity = datetime.datetime.now())
 
 
 
@@ -319,8 +319,6 @@ class GeneralTipsPage(webapp2.RequestHandler):
         newuser = CssiUser(user_name = username, last_activity = datetime.datetime.now())
 
 app = webapp2.WSGIApplication([
-  # ('/SignUp', LogInPage),
-  # ('/login', LogInPage),
   ('/welcome', MainHandler),
   ('/Home', HomePage),
   ('/Education', EducationPage),
