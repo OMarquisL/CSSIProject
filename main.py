3cb77fe99ae2836bb1d14b3322a871175b58d1cd
Garoto0518 <gabrielrguez0518@gmail.com>
2018-07-26 19:13:27 -0700

    Registration Working

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import webapp2
 import jinja2
 import os
 import time
+import logging
+
+jinja_current_dir = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape = True)
 
 EDUCATION_NAV = [
     # Button id      link path           display name
@@ -43,97 +49,100 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 
 class CssiUser(ndb.Model):
-  """CssiUser stores information about a logged-in user.
 
-  The AppEngine users api stores just a couple of pieces of
-  info about logged-in users: a unique id and their email address.
-
-  If you want to store more info (e.g. their real name, high score,
-  preferences, etc, you need to create a Datastore model like this
-  example).
-  """
   first_name = ndb.StringProperty()
   last_name = ndb.StringProperty()
-
-
-jinja_current_dir = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions = ['jinja2.ext.autoescape'],
-    autoescape = True)
+  username = ndb.StringProperty()
+  email = ndb.StringProperty()
+  password = ndb.StringProperty()
 
 class MainHandler(webapp2.RequestHandler):
 
   def get(self):
-    welcome_template = jinja_current_dir.get_template("Templates/index.html")
-    self.response.write(welcome_template.render())
-    user = users.get_current_user()
+    welcome_template = jinja_current_dir.get_template("Templates/signup_page.html")
+    if self.request.cookies.get("loggen_in") == True:
 
-    # If the user is logged in...
-    if user:
-      email_address = user.nickname()
-      cssi_user = CssiUser.get_by_id(user.user_id())
-      signout_link_html = '<a href="%s">sign out</a>' % (
-          users.create_logout_url('/'))
-      # If the user has previously been to our site, we greet them!
-      if cssi_user:
-        self.response.write('''
-            Welcome %s %s (%s)! <br> %s <br>''' % (
-              cssi_user.first_name,
-              cssi_user.last_name,
-              email_address,
-              signout_link_html))
-      # If the user hasn't been to our site, we ask them to sign up
-      else:
-        self.response.write('''
-            Welcome to our site, %s!  Please sign up! <br>
-            <form method="post" action="/">
-            <input type="text" name="first_name">
-            <input type="text" name="last_name">
-            <input type="submit">
-            </form><br> %s <br>
-            ''' % (email_address, signout_link_html))
-    # Otherwise, the user isn't logged in!
+        self.response.write(welcome_template.render(success =True, user = self.request.cookies.get("user")))
+
     else:
-      self.response.write('''
-        Please log in to use our site! <br>
-        <a href="%s">Sign in</a>''' % (
-          users.create_login_url('/')))
-
+        self.response.write(welcome_template.render(failure = True))
+    # If the user is logged in...
   def post(self):
-    bye_template = jinja_current_dir.get_template("Templates/Home.html")
-    self.response.write(bye_template.render())
-    user = users.get_current_user()
-    if not user:
-      # You shouldn't be able to get here without being logged in
-      self.error(500)
-      return
+    home_template = jinja_current_dir.get_template("Templates/signup_page.html")
     cssi_user = CssiUser(
-        first_name=self.request.get('first_name'),
-        last_name=self.request.get('last_name'),
-        id=user.user_id())
+    first_name = self.request.get('firstName'),
+    last_name = self.request.get('lastName'),
+    username = self.request.get('Username'),
+    email = self.request.get('Email'),
+    password = self.request.get('Password'))
+
     cssi_user.put()
-    self.response.write('Thanks for signing up, %s!' %
-        cssi_user.first_name)
+    self.response.set_cookie("logged_in", "True")
+    self.response.set_cookie("user", cssi_user.username)
+    self.response.write(home_template.render(success = True, user = cssi_user.first_name))
+
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        session = jinja_current_dir.get_template("Templates/signIn_page.html")
+        self.response.write(session.render(start = True, error = False))
+
+    def post(self):
+        username = self.request.get("Username")
+        password = self.request.get("Password")
+
+        session_iniciada = False
+        q = CssiUser.query().fetch()
+        for user in q:
+            if (user.username == username and user.password == password) or (user.email == username and user.password == password):
+
+                self.response.set_cookie("loggen_in","True")
+                self.response.set_cookie("user", user.username)
+                self.response.clear()
+                session_iniciada = True
+                break
+
+            if user.username != username or user.password != password or user.email != username:
+                session_iniciada = False
+                self.response.delete_cookie("logged_in")
+                self.response.delete_cookie("user")
+
+        if not session_iniciada:
+            not_session = jinja_current_dir.get_template("Templates/signIn_page.html")
+            self.response.write(not_session.render(start = True, error = True, Username = username, Password = password))
+
+        else:
+            self.redirect("/Home")
+
+class LogoutHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.delete_cookie("logged_in")
+        self.response.delete_cookie("user")
+
+        self.redirect('/')
+
 
 class Dashboard(ndb.Model):
     button_save = ndb.StringProperty();
     actual_name = ndb.StringProperty();
 
 
-class HomePage(webapp2.RequestHandler):
+class HomeWithDashboardPage(webapp2.RequestHandler):
     def get(self):
         home_template = jinja_current_dir.get_template("Templates/homePage.html")
 
-        intento = Dashboard.query().fetch()
-        self.response.write(home_template.render(intento=intento))
-    #def post(self):
+        if self.request.cookies.get("logged_in") == "True":
+            self.response.write(home_template.render(active = True))
+
+        else:
+            self.response.write(home_template.render(login = True))
+
+        self.response.write(home_template.render())
     def post(self):
         answer = self.request.get('link')
         actual_name = self.request.get('actual_name')
 
         SaveData = Dashboard(button_save = answer, actual_name = actual_name)
-
-
 
         SaveData.put()
         self.redirect('/Home')
@@ -143,13 +152,7 @@ class EducationPage (webapp2.RequestHandler):
         page_content = jinja_current_dir.get_template("Templates/education.html")
         params = {
             'navbar_content': EDUCATION_NAV
-            # [
-            #     ("Daycare",  "DaycareandPreschool", "Daycare and Preschool"),
-            #     # "Secondary School",
-                # "Colleges and Universities",
-                # "International Boarding Schools",
-                # "Learning Resources"
-            # ]
+
         }
         self.response.write(page_content.render(params))
 
@@ -291,10 +294,10 @@ class GeneralTipsPage(webapp2.RequestHandler):
         page_content = jinja_current_dir.get_template("Templates/genTips_page.html")
         self.response.write(page_content.render(navbar_content = USCULTURE_NAV))
 
-class LogInPage(webapp2.RequestHandler):
-    def get(self):
-        login_content = jinja_current_dir.get_template("Templates/login_page.html")
-        self.response.write(login_content.render())
+# class LogInPage(webapp2.RequestHandler):
+#     def get(self):
+#         login_content = jinja_current_dir.get_template("Templates/login_page.html")
+#         self.response.write(login_content.render())
 
 
 class ForumPage(webapp2.RequestHandler):
@@ -307,9 +310,9 @@ class FormSubmit(webapp2.RequestHandler):
         print self.request.get("firstname")
 
 app = webapp2.WSGIApplication([
-  ('/Login', LogInPage),
+  ('/Login', LoginHandler),
   ('/', MainHandler),
-  ('/Home', HomePage),
+  ('/Home', HomeWithDashboardPage),
   ('/Education', EducationPage),
   ('/Immigration', ImmigrationPage),
   ('/USLife', USLifePage),
